2431e237db2b79da77c69d531e6eee67899e748b
haslazaro-debug <haslazaro@gmail.com>
2026-09-08 12:37:49 +0500

    Update router.tsx

--- src/router.tsx
+++ src/router.tsx
@@ -1,12 +1,16 @@
 import { QueryClient } from "@tanstack/react-query";
-import { createRouter } from "@tanstack/react-router";
+import { createRouter, createHashHistory } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
+
+// Создаем хэш-историю специально для работы внутри Electron
+const hashHistory = createHashHistory();
 
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
   const router = createRouter({
     routeTree,
+    history: hashHistory, // <-- Добавили эту строчку
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
